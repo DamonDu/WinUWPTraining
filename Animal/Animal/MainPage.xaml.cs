95b1964bd27fa32b0d7d9aec61e837533ea33e95
DamonDu <sy20030260@163.com>
2017-03-10 01:26:32 +0800

    UWP Training
    SYSU 2017 现代操作系统应用开发
    BY: Damon Du [ Du Yi Ke ]

---
+++ Animal/Animal/MainPage.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Animal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private delegate void AnimalSay(object sender);
+        private event AnimalSay Say;
+
+        public MainPage()
+        {  
+            this.InitializeComponent();
+        }
+
+        interface Animal
+        {
+            void say(object sender);
+        }
+
+        class cat : Animal
+        {
+            TextBlock word;
+
+            public cat(TextBlock w)
+            {
+                this.word = w;
+            }
+            public void say(object sender)
+            {
+                this.word.Text += "Cat: I am a Cat" + "\n";
+            }
+        }
+
+        class dog : Animal
+        {
+            TextBlock word;
+
+            public dog(TextBlock w)
+            {
+                this.word = w;
+            }
+            public void say(object sender)
+            {
+                this.word.Text += "Dog: I am a Dog" + "\n";
+            }
+        }
+
+        class pig : Animal
+        {
+            TextBlock word;
+
+            public pig(TextBlock w)
+            {
+                this.word = w;
+            }
+            public void say(object sender)
+            {
+                this.word.Text += "Pig: I am a Pig" + "\n";
+            }
+        }
+
+        private cat c;
+        private dog d;
+        private pig p;
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            c = new cat(textBlock);
+            d = new dog(textBlock);
+            p = new pig(textBlock);
+            Random ran = new Random();
+            int RandKey = ran.Next(1, 4);
+            switch(RandKey)
+            {
+                case 1:
+                    Say += new AnimalSay(c.say);
+                    break;
+                case 2:
+                    Say += new AnimalSay(d.say);
+                    break;
+                case 3:
+                    Say += new AnimalSay(p.say);
+                    break;
+            }
+            Say(this);
+            Say = null;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            String theAnimal = textBox.Text;
+            switch(theAnimal)
+            {
+                case "cat":
+                    Say += new AnimalSay(c.say);
+                    break;
+                case "dog":
+                    Say += new AnimalSay(d.say);
+                    break;
+                case "pig":
+                    Say += new AnimalSay(p.say);
+                    break;
+                default:
+                    break;
+            }
+            Say(this);
+            Say = null;
+            textBox.Text = "";
+        }
+    }
+}
---
+++ Todo/Todo/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace Todo
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            var viewTitleBar = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TitleBar;
+            viewTitleBar.BackgroundColor = Windows.UI.Colors.ForestGreen;
+            viewTitleBar.ButtonBackgroundColor = Windows.UI.Colors.ForestGreen;
+        }
+
+        private void AddAppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(BlankPage1));
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if(CheckBox.IsChecked == true)
+            {
+                Line.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Line.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void CheckBox_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (CheckBox1.IsChecked == true)
+            {
+                Line1.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Line1.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
