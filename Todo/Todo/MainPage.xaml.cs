c9f3fd60b32b92946f639f3fa906bcf2f2a4e6ce
DamonDu <sy20030260@163.com>
2017-03-14 17:16:15 +0800

    20170314

--- Todo/Todo/MainPage.xaml.cs
+++ Todo/Todo/MainPage.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -56,6 +60,82 @@ namespace Todo
             else
             {
                 Line1.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack();
+            }
+        }
+
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime today = DateTime.Now;
+
+            if (title.Text == "" || details.Text == "")
+            {
+                displayTextErrorDialog();
+            }
+            else if (datePicker.Date < today)
+            {
+                displayDateErrorDialog();
+            }
+            else
+            {
+                if (Frame.CanGoBack)
+                {
+                    Frame.GoBack();
+                }
+            }
+        }
+
+        private async void displayTextErrorDialog()
+        {
+            ContentDialog textErrorDialog = new ContentDialog()
+            {
+                Title = "Error",
+                Content = "Please fill all the blank!",
+                PrimaryButtonText = "Ok"
+            };
+
+            ContentDialogResult result = await textErrorDialog.ShowAsync();
+        }
+
+        private async void displayDateErrorDialog()
+        {
+            ContentDialog dateErrorDialog = new ContentDialog()
+            {
+                Title = "Error",
+                Content = "Please choose the right date!",
+                PrimaryButtonText = "Ok"
+            };
+
+            ContentDialogResult result = await dateErrorDialog.ShowAsync();
+        }
+
+        async void SelectPictureButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".jpeg");
+            openPicker.FileTypeFilter.Add(".png");
+
+            StorageFile file = await openPicker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                using (IRandomAccessStream fileStream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+                {
+                    // Set the image source to the selected bitmap 
+                    BitmapImage bitmapImage = new BitmapImage();
+                    await bitmapImage.SetSourceAsync(fileStream);
+                    selectPicture.Source = bitmapImage;
+                }
             }
         }
     }
